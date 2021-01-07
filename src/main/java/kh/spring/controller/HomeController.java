6951e02e3a383ad884c5cef2f8ba087e9fb3fb28
ph <ph@ph.ph>
2021-01-07 14:05:28 +0900

    outputcontroller & jsp add

--- src/main/java/kh/spring/controller/HomeController.java
+++ src/main/java/kh/spring/controller/HomeController.java
@@ -2,38 +2,36 @@ package kh.spring.controller;
 
 import java.text.DateFormat;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-/**
- * Handles requests for the application home page.
- */
+
 @Controller
 public class HomeController {
-	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
+
+	@Autowired
+	private OutputService oService;
+
+	@RequestMapping("/")
 	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
+
+
 		return "home";
 	}
-	
+
+	@RequestMapping("output.io")
+	public String output(Model model) {
+		System.out.println("output 요청 확인");
+		List<PersonDTO> list = oService.select();
+		model.addAttribute("list", list);
+		return "output";	
+	}
 }
