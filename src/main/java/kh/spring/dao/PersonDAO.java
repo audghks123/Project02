f5149472b91f424e82ac1711e3c8fb4e6f72badb
audghks <audghks@h.com>
2021-01-07 14:15:07 +0900

    Person DAO , Mybatis

--- src/main/java/kh/spring/dao/PersonDAO.java
+++ src/main/java/kh/spring/dao/PersonDAO.java
@@ -1,10 +1,10 @@
 package kh.spring.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-
-import kh.spring.vo.MembersVO;
 
 @Repository
 public class PersonDAO {
@@ -16,6 +16,9 @@ public class PersonDAO {
 		}
 	public PersonDTO select()throws Exception{
 		return db.select("Person.select");
+	}
+	public List<PersonDTO> selectList()throws Exception{
+		return db.selectList("Person.select");
 	}
 
 }
